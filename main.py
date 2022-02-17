bc01cc3db1532a13fd3f4ef98501c669645d5308
KonstantyOdwazny <61802175+KonstantyOdwazny@users.noreply.github.com>
2022-02-17 10:57:12 +0100

    Podział kodu na mniejsze funkcje

--- main.py
+++ main.py
@@ -13,6 +13,41 @@ from detecto import core, utils
 from detecto.visualize import show_labeled_image
 import PIL
 
+def load(ann_path,img_path):
+    """
+    Funkcja służąca do odczytu danych z pliku xml
+    """
+    # otwarcie listy plikow xml
+    ann_path_list = get_file_list(ann_path, '.xml')
+    ilosc_obrazow = 0
+    data_path = []
+    data_filename = []
+    data_labels = []
+    for a_path in ann_path_list:
+        ilosc_obrazow = ilosc_obrazow + 1
+        root = ET.parse(a_path).getroot()
+        # Odczytywanie sciezki do zdjecia
+        path_f = Path(str(img_path) + '/' + root.find("./filename").text)
+        data_path.append(path_f)
+        # Odczyt nazwy zdjecia
+        filename = root.find("./filename").text
+        data_filename.append(filename)
+        #Odnajdywanie wszystkich etykiet klas dla danego obrazka
+        labels = []
+        for cl in root.findall("./object/name"):
+            labels.append(cl.text)
+        data_labels.append(labels)
+
+    return ilosc_obrazow, data_path, data_filename, data_labels
+def train_model(labels,dataset):
+    """
+    Funkcja ktora przyjmuje slownik klas labels oraz dane sciezki do etykiet i obrazow
+    Tworzy model za pomoca biblioteki detecto
+    """
+    model = Model(labels)
+    model.fit(dataset)
+    model.save('sings_rmodel.pth')
+    return
 
 def display(paths, boxes, labels):
     image = read_image(paths)
@@ -20,79 +55,77 @@ def display(paths, boxes, labels):
 def get_file_list(root, file_type):
     return [os.path.join(directory_path, f) for directory_path, directory_name,
             files in os.walk(root) for f in files if f.endswith(file_type)]
-def test(ann_path, img_path, model):
+def image_stats(filename, i, xmin, xmax, ymin, ymax):
     """
-    Otwarcie pliku xml i wydobycie z nich potrzebnych informacji jak rzeczywiste klasy znakow
-    Zdobycie informacji : Filename, patch, xmin,xmax,ymin,ymax,width, height, class
+    Funkcja do wyswietlania statysyk znalezionych obrazow wedlug wzorca:
+    file_1
+    n_1
+    xmin_1_1 xmax_1_1 ymin_1_1 ymax_1_1
+    Gdzie :
+    file_1 - nazwa 1. zdjęcia,
+    n_1 - liczba obiektów wykrytych na 1. zdjęciu,
+    xmin_1_1 xmax_1_1 ymin_1_1 ymax_1_1 - współrzędne prostokąta zawierającego pierwszy wykryty obiekt na 1. zdjęciu.
     """
-    ann_path_list = get_file_list(ann_path, '.xml')  #otwarcie listy plikow xml
+    print("Filename:", filename)
+    print("n:", i)
+    for x in range(len(ymax)):
+        print("xmin:", xmin[x], "xmax:", xmax[x], "ymin:", ymin[x], "ymax:", ymax[x])
+
+    return
+def test(ilosc, path_f, filename, real_labels, model):
+    """
+    Testowanie modelu
+    """
+    # ann_path_list = get_file_list(ann_path, '.xml')  #otwarcie listy plikow xml
     TP = 0 #Liczba prawdziwie pozytywnych zdjec
     FN = 0 #Liczba falszywie pozytywnych zdjec
     corr = [] #Lista wykrytych zdjec
 
     Precision = 0
-    for a_path in ann_path_list:
-        root = ET.parse(a_path).getroot()  #odczyt pliku xml
-
-        path_f = Path(str(img_path) + '/' + root.find("./filename").text)
-        size_image = PIL.Image.open(str(path_f))
-        width, height = size_image.size
-        # width = int(root.find("./size/width").text)
-        # height = int(root.find("./size/height").text)
-        filename = root.find("./filename").text
-        i = 0
-        predict_labels = [] #lista predykowanych klas
-        for cl in root.findall("./object/name"):
-            # print(cl.text)
-            predict_labels.append(cl.text)
+    for id in range(ilosc):
+        # Zmienne pomocniczne
         xmin_list = []
-        # for xmin in root.findall("./object/bndbox/xmin"):
-        #     # print(xmin.text)
-        #     xmin_list.append(int(xmin.text))
         ymin_list = []
-        # for ymin in root.findall("./object/bndbox/ymin"):
-        #     # print(ymin.text)
-        #     ymin_list.append(int(ymin.text))
         xmax_list = []
-        # for xmax in root.findall("./object/bndbox/xmax"):
-        #     # print(xmax.text)
-        #     xmax_list.append(int(xmax.text))
         ymax_list = []
-        # for ymax in root.findall("./object/bndbox/ymax"):
-        #     # print(ymax.text)
-        #     ymax_list.append(int(ymax.text))
+        i = 0
+        # #Wydobywanie wielkosci danego obrazu nie korzystajac z plikow xml
+        size_image = PIL.Image.open(str(path_f[id]))
+        width, height = size_image.size
         """
         Predykcja
         """
-        labels = []
+        predict_labels = []
         boxes = []
         scores = []
-        image = read_image(str(path_f)) #odczyt obrazka z sciezki
+        image = utils.read_image(str(path_f[id])) #odczyt obrazka z sciezki
         klasy, ramki, wyniki = model.predict(image) #funkcja sluzaca do obliczenia wartosci prawdopodobienstwa wystepowania znaku o danej wielkosci i klasie
-        # print("labels", klasy)
+        # print("predict_labels", klasy)
         # print("boxes", ramki)
         # print("scores", wyniki)
         for j in range(len(klasy)):
-            if wyniki[j] >= 0.6:
-                labels.append(klasy[j])
-                scores.append(wyniki[j])
-                boxes.append(ramki[j])
-                i = i + 1
-                xmin_list.append(int(ramki[j][0]))
-                xmax_list.append(int(ramki[j][2]))
-                ymin_list.append(int(ramki[j][1]))
-                ymax_list.append(int(ramki[j][3]))
+            if klasy is not None and ramki is not None and wyniki is not None:
+                if wyniki[j] >= 0.6:
+                    predict_labels.append(klasy[j])
+                    scores.append(wyniki[j])
+                    boxes.append(ramki[j])
+                    i = i + 1
+                    xmin_list.append(int(ramki[j][0]))
+                    xmax_list.append(int(ramki[j][2]))
+                    ymin_list.append(int(ramki[j][1]))
+                    ymax_list.append(int(ramki[j][3]))
 
-        print("Filename:", filename)
-        print("n:", i)
-        for x in range(len(ymax_list)):
-            print("xmin:", xmin_list[x], "xmax:", xmax_list[x], "ymin:", ymin_list[x], "ymax:", ymax_list[x])
+        # print("Filename:", filename[id])
+        # print("n:", i)
+        # for x in range(len(ymax_list)):
+        #     print("xmin:", xmin_list[x], "xmax:", xmax_list[x], "ymin:", ymin_list[x], "ymax:", ymax_list[x])
+        image_stats(filename[id], i, xmin_list, xmax_list, ymin_list, ymax_list)
 
         """ Ewaluacja oraz wyswietlanie poprawnie sklasyfikowanych obrazow """
         score = [] # Wynik prawdopodobienstwa wystepowania znaku z danej klasy
         indeks = []
         for s in range(len(scores)):
-            if labels[s] == 'crosswalk':
+            if predict_labels[s] == 'crosswalk':
                 dlugosc_znaku = xmax_list[s] - xmin_list[s]
                 wysokosc_znaku = ymax_list[s] - ymin_list[s]
                 if dlugosc_znaku/width >= 0.1 and wysokosc_znaku/height >= 0.1:
@@ -103,26 +136,22 @@ def test(ann_path, img_path, model):
         pom_tp = TP
         if indeks is not None:
             for j in range(len(indeks)):
-                for z in range(len(predict_labels)):
-                    if predict_labels[z] == labels[indeks[j]]:
+                for z in range(len(real_labels[id])):
+                    if real_labels[id][z] == predict_labels[indeks[j]]:
                         TP = TP + 1
-                        display(str(path_f),boxes[indeks[j]],labels[indeks[j]]) # wyswietlanie poprawnych zdjec
-                        corr.append(filename)
+                        display(str(path_f[id]),boxes[indeks[j]],predict_labels[indeks[j]]) # wyswietlanie poprawnych zdjec
+                        corr.append(filename[id])
                 if pom_tp == TP:
                     FN = FN + 1
 
-
-
-    if TP != 0 and FN != 0:
-        Precision = TP / (TP + FN)
-    else:
-        Precision = 0
-
+    Precision = TP/(TP + FN)
     print("Lista wykrytych zdjec:", corr)
     return Precision
 
-
 def main():
+    """
+    Sciezki dostepu do plików
+    """
     base_path = 'train/'
     ann_path = base_path + 'annotations/'
     img_path = base_path + 'images/'
@@ -130,17 +159,23 @@ def main():
     test_ann_path = test_base_path + 'annotations/'
     test_img_path = test_base_path + 'images/'
 
+    # Słownik klas
     labels = ['speedlimit', 'stop', 'crosswalk', 'trafficlight']
+
+    # Tworzenie sciezki do danych etykiet oraz obrazow dla wyuczenia modelu
+    dataset = Dataset(ann_path, img_path)
+
     """
     Uzywane tylko raz podczas trenowania
     """
-    # dataset = Dataset(ann_path, img_path)
-    # model = Model(labels)
-    # model.fit(dataset)
-    # model.save('sings_rmodel.pth')
+    # print("Trenowanie")
+    # train_model(labels,dataset)
 
+    # Wczytanie wyuczonego modelu
     model = core.Model.load('sings_rmodel.pth', labels)
-    score = test(test_ann_path, test_img_path, model)
+
+    liczba_obrazow, test_path, test_filename, test_labels = load(test_ann_path, test_img_path)
+    score = test(liczba_obrazow, test_path, test_filename, test_labels, model)
     print("Score = ", score)
     return
 if __name__ == '__main__':
